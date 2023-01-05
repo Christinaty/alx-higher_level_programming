2063063ac204195d1568555abba7f7497c6089e6
Christina <111030842+Christinaty@users.noreply.github.com>
2023-01-05 19:30:30 +0100

    python driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-
-"""Defines a name-printing function."""
-
-
-
+"""
+Function to print a phrase
+"""
 
 
 def say_my_name(first_name, last_name=""):
-
-    """Print a name.
-
-    Args:
-
-        first_name (str): The first name to print.
-
-        last_name (str): The last name to print.
-
-    Raises:
-
-        TypeError: If either of first_name or last_name are not strings.
-
     """
-
-    if not isinstance(first_name, str):
-
+    Prints a phrase with:
+    first_name: string to be printed
+    last_name: second string to be printed
+    Return: A formatted output with the strings.
+    """
+    if type(first_name) not in [str]:
         raise TypeError("first_name must be a string")
-
-    if not isinstance(last_name, str):
-
+    if type(last_name) not in [str]:
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
